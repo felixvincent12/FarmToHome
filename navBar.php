e9ab8b84267820248d1252f304ce2b793a03a73c
feliciandayani <feliciandayani1627@gmail.com>
2018-05-09 00:29:27 +0800

    About us page
    not complete.. require further modification on team description and company mission

--- navBar.php
+++ navBar.php
@@ -26,7 +26,7 @@
                     <ul class="nav navbar-nav">
                         <li><a href="ProductPage.php">Product</a></li>
                         <li><a href="ProductManagementPage.php">Sell Product</a></li>
-                        <li><a href="#">About Us</a></li>
+                        <li><a href="aboutUs.php">About Us</a></li>
                         <li><a href="FeedbackPage.php">Give Feedback</a></li>
                         <li data-ng-show="login == false"><a href="SignUpPage.php">Register</a></li>
                     </ul>
