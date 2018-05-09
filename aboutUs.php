d4f7271722b8f253888a161a5aba79513ed3850b
FelixVincent12 <100079233@students.swinburne.edu.my>
2018-05-09 17:42:05 +0800

    modify about us page

--- aboutUs.php
+++ aboutUs.php
@@ -18,7 +18,7 @@
         <link type="text/css" href="css/style.css" rel="stylesheet"/>
     </head> 
     
-    <body>
+    <body style="background-image: url('image/aboutus-bg.jpg');">
         
         
         <!-- row 1: for navigation bar-->
@@ -33,22 +33,27 @@
         
         <!-- content-->
         <div class="row pushNav">
-            <div class="container">
+            <div class="container" style="background-color: #f2f2f2; opacity: 0.9;">
             <div class="col-md-12">
                 <h1 class="text-center">About Us</h1>
-                <hr>
+                <hr style="min-height: 2px; background-color: black;"/>
                 <!-- description -->
-                 <div class="row">
+                 <div class="row" style="background-color: #ffffe6;">
                     <div class="col-md-12" id="aboutus">
-                        <p>FarmtoHome is a first web application created to connect customers directly with farmers. FarmtoHome offers higher profit to farmers who decides to join and also flexible groceries shopping for the customers. All orders made will be delivered within 2 days by our special delivery couriers from the farmer to the customers' door. All groceries are ensured to be fresh and safe as well as well-packed before it is delivered to the customer.</p> 
+                        <blockquote class="text-info text-justify">FarmToHome is a first web application created to connect customers directly with farmers. FarmtoHome offers higher profit to farmers who decides to join and also flexible groceries shopping for the customers. All orders made will be delivered within 2 days by our special delivery couriers from the farmer to the customers' door. All groceries are ensured to be fresh and safe as well as well-packed before it is delivered to the customer.</blockquote> 
                     </div>
                 </div>
                 <!-- Mission -->
                 <br />
                 <div class="row">
+                    <div class="col-md-12">
+                        
+                    </div>
+                </div>
+                <div class="row" style="background-color: #ffffe6;">
                     <div class="col-md-12" id="mission">
                         <h3 class="text-center" style="font-weight:bold">Our Mission</h3>
-                        <p>Our mission is to provide better groceries shopping and selling methods between the farmers and the customers by utilizing the existing technology.</p> 
+                        <blockquote class="text-info">Our mission is to provide better groceries shopping and selling methods between the farmers and the customers by utilizing the existing technology.</blockquote> 
                     </div>
                 </div>
                 <!-- Meet the team -->
@@ -56,18 +61,18 @@
                     <h3 class="text-center" style="font-weight:bold">Meet The Team</h3>
                     <div class="col-md-4 text-center team">
                         <img src="image/guy.png" class="img-circle" alt="Felix" width="200px" height="200px"/>
-                        <h3>Felix Vincent<br/><small>Founder</small></h3>
-                        <p>Description</p>
+                        <h3>Felix Vincent<br/><small>Co-Founder</small></h3>
+                        <p>Student in Swinburne University Technology Sarawak</p>
                     </div>
                     <div class="col-md-4 text-center team">
                         <img src="image/girl.png" class="img-circle" alt="Felix" width="200px" height="200px"/>
-                        <h3>Felicia Andayani<br/><small>Founder</small></h3>
-                        <p>Description</p>
+                        <h3>Felicia Andayani<br/><small>Co-Founder</small></h3>
+                        <p>Student in Swinburne University Technology Sarawak</p>
                     </div>
                     <div class="col-md-4 text-center team">
                         <img src="image/guy.png" class="img-circle" alt="Felix" width="200px" height="200px"/>
-                        <h3>Lee Sing Hong<br/><small>Founder</small></h3>
-                        <p>Description</p>
+                        <h3>Lee Sing Hong<br/><small>Co-Founder</small></h3>
+                        <p>Student in Swinburne University Technology Sarawak</p>
                     </div>
                 </div>
             </div>
